f28f87f2470c52ae04b71d8db39c8b8272b92a16
BenoniM <benonimoges01@gmail.com>
2026-04-15 12:08:55 +0300

    some more

--- src/components/SmoothScroll.jsx
+++ src/components/SmoothScroll.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react'
-import Lenis from 'lenis'
+import Lenis from '@studio-freight/lenis'
 import { gsap } from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
